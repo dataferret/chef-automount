80d70374d03a93d9170bd84b402fbb705a75a70d
Dan Rice <dan@zoombody.com>
2016-12-15 11:29:43 -0500

    Make compatible with Chef 11

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email 'datacoda@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures automount'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.2.2'
-source_url 'https://github.com/datacoda/chef-automount'
-issues_url 'https://github.com/datacoda/chef-automount/issues'
+version '0.2.3'
+source_url 'https://github.com/datacoda/chef-automount' if respond_to?(:source_url)
+issues_url 'https://github.com/datacoda/chef-automount/issues' if respond_to?(:issues_url)
 
 %w(ubuntu).each do |os|
   supports os
